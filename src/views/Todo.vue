9172d604d4a6879502019afc87ac90d2accf713d
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-03-30 16:12:32 -0300

    Finished todo app basic

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
     <v-list class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
         <v-list-item
@@ -20,7 +31,7 @@
             </v-list-item-content>
 
             <v-list-item-action>
-              <v-btn icon>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
                 <v-icon color="grey lighten-1">mdi-delete</v-icon>
               </v-btn>
             </v-list-item-action>
@@ -37,29 +48,28 @@ export default {
   name: "Home",
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
-        {
-          id: 1,
-          title: "Wake up",
-          done: false,
-        },
-        {
-          id: 2,
-          title: "Run 2km",
-          done: false,
-        },
-        {
-          id: 3,
-          title: "Do homework",
-          done: false,
-        },
-      ],
+        
+      ]
     };
   },
   methods: {
+    addTask(){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
