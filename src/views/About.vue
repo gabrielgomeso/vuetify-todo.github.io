96b9fd3224d7e322c99e02d5ca8299c2f3ea3bdb
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-03-30 12:43:43 -0300

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores rerum totam suscipit quod. Officia dolorem excepturi ut, provident at ipsa cumque atque quo sed ullam. Corrupti sapiente praesentium voluptatem repellat.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores rerum totam suscipit quod. Officia dolorem excepturi ut, provident at ipsa cumque atque quo sed ullam. Corrupti sapiente praesentium voluptatem repellat.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores rerum totam suscipit quod. Officia dolorem excepturi ut, provident at ipsa cumque atque quo sed ullam. Corrupti sapiente praesentium voluptatem repellat.</p>
   </div>
 </template>
